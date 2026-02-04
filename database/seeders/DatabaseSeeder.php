337eee9f6510b700e84972c0a4da448f985ebd40
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-04 13:00:26 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,19 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // 1. Buat Departemen
-        $deptIT = Department::create(['code' => 'IT', 'name' => 'Information Technology']);
-        $deptFin = Department::create(['code' => 'FIN', 'name' => 'Finance & Accounting']);
+        $deptIT = Department::create(['code' => '5IT', 'name' => 'INFORMATION TECHNOLOGY']);
+        $deptFin = Department::create(['code' => '5FA', 'name' => 'FINANCE & ACCOUNTING']);
 
-        // 2. Buat COA (Chart of Accounts)
-        $coa1 = Coa::create(['code' => '6001', 'name' => 'Biaya Transportasi']);
-        $coa2 = Coa::create(['code' => '6002', 'name' => 'Biaya Konsumsi']);
-        $coa3 = Coa::create(['code' => '6003', 'name' => 'Pembelian ATK']);
 
         // Hubungkan COA ke Departemen (Many-to-Many)
         // IT boleh pakai: Transport & ATK
-        $deptIT->coas()->attach([$coa1->id, $coa3->id]);
-        // Finance boleh pakai semua
-        $deptFin->coas()->attach([$coa1->id, $coa2->id, $coa3->id]);
+
 
         // 3. Buat User untuk Simulasi Approval
 
