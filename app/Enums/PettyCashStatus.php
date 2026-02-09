9d9ae61af99bbcfdc6f55746958362237ce5de41
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-09 09:56:13 +0700

    added approval for HC and Klinik

--- app/Enums/PettyCashStatus.php
+++ app/Enums/PettyCashStatus.php
@@ -5,9 +5,12 @@ namespace App\Enums;
 enum PettyCashStatus: string
 {
     case DRAFT = 'draft';
+    case PENDING_SUPERVISOR = 'pending_supervisor';
     case PENDING_MANAGER = 'pending_manager';
     case PENDING_DIRECTOR = 'pending_director';
     case PENDING_FINANCE = 'pending_finance';
+    case PENDING_KLINIK = 'pending_klinik';
+    case PENDING_HC = 'pending_hc';
     case PAID = 'paid';
     case REJECTED = 'rejected';
 
@@ -15,9 +18,12 @@ enum PettyCashStatus: string
     {
         return match ($this) {
             self::DRAFT => 'Draft',
-            self::PENDING_MANAGER => 'Menunggu Manager Approval',
-            self::PENDING_DIRECTOR => 'Menunggu Director Approval',
-            self::PENDING_FINANCE => 'Menunggu Finance Approval',
+            self::PENDING_SUPERVISOR => 'Menunggu Supervisor',
+            self::PENDING_MANAGER => 'Menunggu Manager',
+            self::PENDING_DIRECTOR => 'Menunggu Director',
+            self::PENDING_HC => 'Menunggu Verifikasi Oleh Human Capital',
+            self::PENDING_KLINIK => 'Menunggu Verifikasi Oleh Klinik',
+            self::PENDING_FINANCE => 'Menunggu Finance Proses',
             self::PAID => 'Selesai / Dibayar',
             self::REJECTED => 'Ditolak',
         };
