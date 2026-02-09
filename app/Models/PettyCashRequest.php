31e6f02d8bf7d20e0ea23dd9a91f30aa531ba46b
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-09 10:07:19 +0700

    added fillable item

--- app/Models/PettyCashRequest.php
+++ app/Models/PettyCashRequest.php
@@ -3,38 +3,63 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Enums\PettyCashStatus;
 use App\Enums\PettyCashType;
 
 class PettyCashRequest extends Model
 {
-    protected $guarded = ['id'];
+    protected $fillable = [
+        'tracking_number',
+        'user_id',
+        'department_id',
+        'title',
+        'description',
+        'amount',
+        'type',
+        'status',
+        'attachment',
+        'extra_attachment',
+    ];
 
-    // Casting otomatis Enum & Tanggal
     protected $casts = [
         'status' => PettyCashStatus::class,
-        'type' => PettyCashType::class,
+        'type'   => PettyCashType::class,
         'amount' => 'decimal:2',
-        'manager_approved_at' => 'datetime',
-        'director_approved_at' => 'datetime',
-        'finance_approved_at' => 'datetime',
+        'supervisor_approved_at' => 'datetime',
+        'manager_approved_at'    => 'datetime',
+        'director_approved_at'   => 'datetime',
+        'finance_approved_at'    => 'datetime',
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+
+            if (empty($model->tracking_number)) {
+
+                $model->tracking_number = 'PC-' . date('Ymd') . '-' . strtoupper(Str::random(4));
+            }
+        });
+    }
+
 
     public function details()
     {
         return $this->hasMany(PettyCashDetail::class);
     }
 
-    // Relasi
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class);
     }
+
     public function coa(): BelongsTo
     {
         return $this->belongsTo(Coa::class);
