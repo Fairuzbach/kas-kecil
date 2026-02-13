e90f34cb6b7163967dd865be3c83959f2b50db21
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-13 15:28:42 +0700

    added manager approver for department

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Department extends Model
 {
     protected $guarded = ['id'];
-
+    protected $fillable = ['name', 'manager_id', 'director_group'];
     public function users()
     {
         return $this->hasMany(User::class);
@@ -25,5 +26,13 @@ class Department extends Model
     public function divisions()
     {
         return $this->hasMany(Division::class);
+    }
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approver_id');
+    }
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'manager_id');
     }
 }
