5ac66d8a305890238033f62dae245c0c92437295
Fairuzbach <fairuzbachri10@gmail.com>
2026-02-13 15:30:22 +0700

    rename department

--- database/seeders/UpdateDirectorGroupSeeder.php
+++ database/seeders/UpdateDirectorGroupSeeder.php
@@ -10,27 +10,27 @@ class UpdateDirectorGroupSeeder extends Seeder
     public function run()
     {
         // 1. Manufacturing Director
-        $manufacturing = ['FIBER OPTIC', 'MAINTENANCE', 'PROCESS ENGINEERING', 'PRODUCTION PLANNING', 'LOW VOLTAGE', 'MEDIUM VOLTAGE', 'FO', 'PP', 'PE', 'LV', 'MV'];
+        $manufacturing = ['FIBER OPTIC MANAGER', 'MAINTENANCE MANAGER', 'PROCESS ENGINEERING MANAGER', 'PRODUCTION PLANNING MANAGER', 'LOW VOLTAGE MANAGER', 'MEDIUM VOLTAGE MANAGER', 'FO', 'PP', 'PE', 'LV', 'MV'];
         Department::whereIn('name', $manufacturing)->orWhereIn('code', $manufacturing)
             ->update(['director_group' => 'manufacturing']);
 
         // 2. Finance Director
-        $finance = ['FINANCE & ACCOUNTING', 'INFORMATION TECHNOLOGY', 'IT', 'FA', 'Finance'];
+        $finance = ['FINANCE & ACCOUNTING MANAGER', 'INFORMATION TECHNOLOGY MANAGER', 'IT MANAGER', 'FA MANAGER', 'Finance'];
         Department::whereIn('name', $finance)->orWhereIn('code', $finance)
             ->update(['director_group' => 'finance']);
 
         // 3. President Director
-        $president = ['QUALITY ASSURANCE & R D', 'FACILITY', 'QA'];
+        $president = ['QUALITY ASSURANCE & R D MANAGER', 'FACILITY MANAGER', 'QA MANAGER'];
         Department::whereIn('name', $president)->orWhereIn('code', $president)
             ->update(['director_group' => 'president']);
 
         // 4. Human Capital Director
-        $hc = ['HUMAN CAPITAL', 'GENERAL AFFAIR', 'HC', 'GA', 'HRD'];
+        $hc = ['HUMAN CAPITAL MANAGER', 'GENERAL AFFAIR MANAGER', 'LEGAL DEPARTMENT', 'RESEARCH & DEVELOPMENT DEPARTMENT'];
         Department::whereIn('name', $hc)->orWhereIn('code', $hc)
-            ->update(['director_group' => 'hc']);
+            ->update(['director_group' => 'commercial']);
 
         // 5. Commercial & Supply Chain Director
-        $commercial = ['SALES', 'SALES 2', 'SALES SUPPORT', 'MARKETING', 'PROCUREMENT'];
+        $commercial = ['SALES MANAGER', 'SALES 2 MANAGER', 'SALES SUPPORT MANAGER', 'MARKETING MANAGER', 'PROCUREMENT MANAGER'];
         Department::whereIn('name', $commercial)->orWhereIn('code', $commercial)
             ->update(['director_group' => 'commercial']);
     }
